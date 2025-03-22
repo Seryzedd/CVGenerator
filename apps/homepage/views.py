67c29b29802b4fd5b64a962a0be0e55caa353dee
Seryzedd <aurelienbard@hotmail.fr>
2025-03-22 14:08:25 +0100

    add django files

---
+++ apps/homepage/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepage_view(request):
+    #return HttpResponse("Bienvenue page accueil !")
+    return render(request, 'home/index.html')
