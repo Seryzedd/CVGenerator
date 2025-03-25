3c30124b7d49c54504d7398e129fc4fd225db774
Seryzedd <aurelienbard@hotmail.fr>
2025-03-25 12:00:51 +0100

    Create CV entity

--- CVGenerator/settings.py
+++ CVGenerator/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.homepage',
-    'apps.login'
+    'apps.login',
+    'apps.CV'
 ]
 
 MIDDLEWARE = [
---
+++ apps/CV/models/CV.py
@@ -0,0 +1,21 @@
+from django.db import models
+from apps.login.models.user import User
+from django.utils.text import slugify
+
+class CV(models.Model):
+    name = models.CharField(max_length=50, unique=False)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    description = models.TextField(max_length=255)
+    template = models.CharField(max_length=100)
+    primaryColor = models.CharField(max_length=80)
+    secondaryColor = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="CVS")
+
+    class Meta:
+        verbose_name = 'CV'
+        verbose_name_plural = "CVS"
+        ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
